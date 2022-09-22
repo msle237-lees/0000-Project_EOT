0c722137c2fc40ebe787079f7abd00928552a091
user <user@users-MBP.hsd1.ga.comcast.net>
2022-09-21 20:05:14 -0400

    Steps 1,2,3,4 done

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,23 @@
 from src import app
 import argparse
+
+arg_desc = '''\
+            0001-Project_EOT
+                End of Term Project for CSE 2300-02 - Discrete Structures.
+                Author: Michael Lees
+            '''
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=arg_desc)
+parser.add_argument("-c", "--code", help="Runs step 1 from the Readme File.", required=False)
+args = parser.parse_args()
+print(args)
+
 if __name__ == '__main__':
-    app.run()
+    if args.code == "step1":
+        app.run_step_1()
+    elif args.code == "step2":
+        app.run_step_2()
+    elif args.code == "step3":
+        app.run_step_3()
+    elif args.code == "step4":
+        app.run_step_4()
--- src/app.py
+++ src/app.py
@@ -1,3 +1,78 @@
-import argparse
-def run():
-    print("Hello World!")
+from time import perf_counter
+import random
+
+def bubbleSort(arr):
+    n = len(arr)
+    swapped = False
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        if not swapped:
+            return
+
+def selectionSort(array, size):
+    for ind in range(size):
+        min_index = ind
+        for j in range(ind + 1, size):
+            if array[j] < array[min_index]:
+                min_index = j
+        (array[ind], array[min_index]) = (array[min_index], array[ind])
+
+def run_step_1():
+    n = int(input("Enter number of elements in array: "))
+    l = []
+    for i in range(n):
+        l.append(random.randint(0, 10000))
+    print(l)
+    t_start = perf_counter()
+    bubbleSort(l)
+    t_stop = perf_counter()
+    print(f"Elapsed Time: {t_stop-t_start}")
+    print(l)
+
+def run_step_2():
+    n = int(input("Enter number of elements in array: "))
+    l = []
+    for i in range(n):
+        l.append(random.randint(0, 10000))
+    print(l)
+    t_start = perf_counter()
+    selectionSort(l, n)
+    t_stop = perf_counter()
+    print(f"Elapsed Time: {t_stop-t_start}")
+    print(l)
+
+def run_step_3():
+    n = int(input("Enter number of elements in array: "))
+    t_times = []
+    for i in range(1000):
+        l = []
+        for i in range(n):
+            l.append(random.randint(0, 10000))
+        t_start = perf_counter()
+        bubbleSort(l)
+        t_stop = perf_counter()
+        t_times.append(t_stop-t_start)
+    t_time = 0
+    for i in range(len(t_times)):
+        t_time += t_times[i]
+    print(f'Elapsed Time: {t_time}')
+
+def run_step_4():
+    n = int(input("Enter number of elements in array: "))
+    t_times = []
+    for i in range(1000):
+        l = []
+        for i in range(n):
+            l.append(random.randint(0, 10000))
+        t_start = perf_counter()
+        selectionSort(l, n)
+        t_stop = perf_counter()
+        t_times.append(t_stop-t_start)
+    t_time = 0
+    for i in range(len(t_times)):
+        t_time += t_times[i]
+    print(f'Elapsed Time: {t_time}')
